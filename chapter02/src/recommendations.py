121707ccc3de2b37ec8f6e80ffcc052962359074
otwm <otwmoo@gmail.com>
2015-12-21 16:53:22 +0900

    항목추천

--- chapter02/src/recommendations.py
+++ chapter02/src/recommendations.py
@@ -78,3 +78,24 @@ def topMatches(prefs, person, n=5, similarity=sim_peason):
     return scores[0:n]
 
 
+def getRecommendations(prefs, person, similarity=sim_peason):
+    totals = {}
+    simSums = {}
+
+    for other in prefs:
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+
+        if sim <= 0: continue
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+
+    rankings.sort()
+    rankings.reverse()
+    return rankings
